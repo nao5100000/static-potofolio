4a39d760199e478644bebfb5f5637d6f429a6bdc
nao <aojima51070@gmail.com>
2023-09-25 12:26:05 +1000

    #1 markup HTML and css for toppage

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@
 const domain = document.domain;
 const regexp = new RegExp(domain);
 const links = document.getElementsByTagName('a');
-for(let link of links) {
-    if(!regexp.test(link.href)) {
-        if(link.href.match(/^https:\/\//) || link.href.match(/\.pdf/)) {
+for (let link of links) {
+    if (!regexp.test(link.href)) {
+        if (link.href.match(/^https:\/\//) || link.href.match(/\.pdf/)) {
             link.setAttribute('target', '_blank');
             link.setAttribute('rel', 'noopener');
         }
@@ -17,24 +17,12 @@ const gnav = document.getElementById('js-global-menu');
 const body = document.querySelector('body');
 let scrollValue;
 
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('open');
-    gnav.classList.toggle('open');
-    body.classList.toggle('open');
-    if(body.classList.contains('open')) {
-        scrollValue = window.pageYOffset;
-        body.style.top = -scrollValue + 'px';
-    }else {
-        body.style.removeProperty('top');
-        window.scrollTo(0, scrollValue);
-    }
-});
 
 // Internal link
 const menus = document.querySelectorAll('.global__menu > ul > li > a');
 menus.forEach(menu => {
     menu.addEventListener('click', () => {
-        if(body.classList.contains('open')) {
+        if (body.classList.contains('open')) {
             body.classList.remove('open');
             gnav.classList.remove('open');
         }
@@ -43,8 +31,30 @@ menus.forEach(menu => {
 
 // Adjust link position
 window.onload = () => {
-    if(window.location.hash == "") {
+    if (window.location.hash == "") {
         return;
     }
     document.getElementById(window.location.hash.slice(1)).scrollIntoView(true);
 }
+
+let y, num, num02;
+const particleW = document.getElementById('js-particle-wrapper');
+const particles = document.querySelectorAll('.particle');
+const topParticle = () => {
+    window.addEventListener('scroll', () => {
+        y = window.scrollY;
+        num = y / 500;
+        num02 = y / 1000
+        particleW.style.cssText = `
+            opacity: ${1 - num};
+            transform: scale3d( ${1.5 + num02}, ${1.5 + num02}, 1);
+        `
+        if(.5 >= particleW.style.opacity){
+            particleW.style.opacity = .5;
+        }
+        if(1.5 + num02 >= 2){
+            particleW.style.transform = scale3d( 2, 2, 1);
+        }
+    })
+}
+topParticle();
